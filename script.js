624146f3dca37cb0d686f87d2602421aa3112b90
Vaibhav Bhagat <vaibhavbhagat7461@gmail.com>
2025-04-27 13:35:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -463,9 +463,24 @@ function updateNavButtons() {
 }
 
 
+<script>
 document.addEventListener('contextmenu', function(e) {
     e.preventDefault();
 });
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === "F12") {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && (e.key.toLowerCase() === 'i' || e.key.toLowerCase() === 'c' || e.key.toLowerCase() === 'j')) {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.key.toLowerCase() === 'u') {
+        e.preventDefault();
+    }
+});
+</script>
+
 
 
 
